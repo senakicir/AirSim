27d3549a2c46fe9ab75b0a53cbab4adea099129f
senakicir <senakicir@gmail.com>
2018-01-30 10:13:43 +0100

    minor change

--- PythonClient/my_scripts/training/__init__.py
+++
@@ -1,6 +0,0 @@
-from .sampler import Sampler
-from .srng import RNG
-from .trainer import Trainer, managed_objects
-from .summary import Summary
-from .learning_rate_scheduler import LearningRateScheduler
-from .utils import makedirs, config_logger
